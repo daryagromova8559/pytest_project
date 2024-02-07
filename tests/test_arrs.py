2477580ea72201ec95831d930b0af2d0c38fb699
dasha gromova <darya.gromova8559@gmail.com>
2024-02-07 14:00:09 +0300

    cov test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -13,3 +13,4 @@ def test_slice():
     assert arrs.my_slice([3, 4, 5, 6, 7], 10) == []
     assert arrs.my_slice([1, 2, 5, 7, 9], None) == [1, 2, 5, 7, 9]
     assert arrs.my_slice([1, 2, 3, 5, 6, 7, 0], -5) == [3, 5, 6, 7, 0]
+    assert arrs.my_slice([1, 2, 3], -3, -1) == [1, 2]
